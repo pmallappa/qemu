9325fe607fafa38f299a89c09b64df1c7f500dab
Prem Mallappa <pmallapp@broadcom.com>
2016-01-01 08:42:36 +0530

    arm: smmu: ARM SMMUv3 emulation
    Implementation Notes:
            - Implements ARM SMMUv3 version 11.0
    	- Works with SMMUv3 Driver in Linux Kernel 4.4
    	- Only LPAE mode Translation supported
    	- BE mode TT not supported Yet
    	- Stage1 only
    	- Suspend/Resume not yet supported
    	- Broadcom variant added, but not much difference at the moment
    
    Signed-off-by: Prem Mallappa <pmallapp@broadcom.com>

---
+++ include/hw/arm/smmuv3.h
@@ -0,0 +1,39 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Copyright (C) 2014-2015 Broadcom Corporation
+ *
+ * Author: Prem Mallappa <pmallapp@broadcom.com>
+ *
+ */
+#ifndef HW_ARM_SMMU_V3_H
+#define HW_ARM_SMMU_V3_H
+
+#define TYPE_SMMU_DEV_BASE "smmuv3-base"
+#define TYPE_SMMU_DEV      "smmuv3"
+#define TYPE_SMMU_BRCM_DEV "smmuv3-brcm"
+
+typedef enum {
+    SMMU_IMPL_ARM = 0x1,
+    SMMU_IMPL_BRCM,
+} SMMUImpl;
+
+typedef enum {
+    SMMU_IRQ_GERROR,
+    SMMU_IRQ_PRIQ,
+    SMMU_IRQ_EVTQ,
+    SMMU_IRQ_CMD_SYNC,
+} SMMUIrq;
+
+#endif
